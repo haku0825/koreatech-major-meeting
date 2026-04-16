ccd4192118c4c5455b6a8eb1ba90fa43fb1cf5e2
songzio <zioziozzang@naver.com>
2026-04-16 14:23:18 +0900

    first commit

---
+++ src/main/java/haku/kmm/org/koreatechmajormeeting/KoreatechMajorMeetingApplication.java
@@ -0,0 +1,13 @@
+package haku.kmm.org.koreatechmajormeeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreatechMajorMeetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoreatechMajorMeetingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/haku/kmm/org/koreatechmajormeeting/KoreatechMajorMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package haku.kmm.org.koreatechmajormeeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreatechMajorMeetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
